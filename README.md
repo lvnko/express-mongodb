fc46aa1679c622eba382df407544ae23a7fb4837
lvnko <kevinlao.lo@gmail.com>
2025-01-17 10:30:30 -0300

    加入 moogoose 筆記。

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Week #009
+# Week #009 + Week #010
 ## 專案啟動步驟
 1. 安裝與設定 mongodb 開發環境，執行步驟可以參考 [``setup.md``](./setup.md)
 2. 安裝專案依賴模組
@@ -11,15 +11,12 @@ npm run start
 ```  
 ### 開發環境依賴
 - Mongodb v8.0
-- Node.js v18.20.2  
+- Node.js v18.20.2
+
+## moogooose 語法
+moogoose 的使用語法與 MongoDB Shell 非常相似，其使用方法可參考另一專案中的 [筆記](../lvnko/graphql-server/blob/main/notes/mongodb-shell.md)。
 
 ## 其他有用資源
-- GraphQL
-    - [AST Explorer](https://astexplorer.net/)
-    - Reading : [GraphQLSchema vs. buildSchema vs. makeExecutableSchema](https://stackoverflow.com/questions/53984094/notable-differences-between-buildschema-and-graphqlschema)
-    - Example : [buildSchema with SDL while defining resolver separately](https://github.com/IvanGoncharov/swapi-demo/blob/master/src/index.ts)
-    - 官方教程 : [GraphQL tutorials and resources](https://www.apollographql.com/tutorials/browse/)
-    - 官方使用手冊 : [Introduction to GraphQL...](https://graphql.org/learn/)
 - MongoDB [[官網](https://www.mongodb.com/)]
     - 使用手冊 (8.0 current) [What is MongoDB?](https://www.mongodb.com/docs/manual/)
 - Mongoose [官網](https://mongoosejs.com/)
--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const db = mongoose.connect('mongodb://localhost:27017/test', {
     // useUnifiedTopology: true
 });
 
-const find = async ({ collectionModel, filter = {} }) => {
+const findAsync = async ({ collectionModel, filter = {} }) => {
     try {
         const result = await collectionModel.find(filter);
         return {
@@ -21,9 +21,39 @@ const find = async ({ collectionModel, filter = {} }) => {
     }
 }
 
-const insertMany = async ({ collectionModel, data }) => {
+const insertAsync = async ({ collectionModel, data }) => {
     try {
         const result = await collectionModel.insertMany(data);
+        return {
+            statusText: 'ok',
+            payload: result
+        };
+    } catch (err) {
+        return {
+            statusText: 'fail',
+            message: err.message
+        };
+    }
+}
+
+const deleteAsync = async ({ collectionModel, conditions, multiple = false }) => {
+    try {
+        const result = multiple ? await collectionModel.deleteMany(conditions) : await collectionModel.deleteOne(conditions);
+        return {
+            statusText: 'ok',
+            payload: result
+        };
+    } catch (err) {
+        return {
+            statusText: 'fail',
+            message: err.message
+        };
+    }
+}
+
+const updateAsync = async ({ collectionModel, filter, update, multiple = false }) => {
+    try {
+        const result = multiple ?  await collectionModel.updateMany(filter, update) : await collectionModel.updateOne(filter, update);
         return {
             statusText: 'ok',
             payload: result
@@ -61,30 +91,59 @@ db.then(async (mongo)=>{
     );
     console.log('Model => ', School);
 
-    const findResult = await find({
+    const findResult = await findAsync({
         collectionModel: School,
         filter: {
-            name: 'Columbia University',
-            countryRegion: 'United States',
-            city: 'New York City'
-        }
+            // name: 'Columbia University',
+            countryRegion: {
+                $in: ['United States', 'U.S.A.']
+            },
+            // city: 'New York City'
+        },
+        
     });
 
     console.log('findResult => ', findResult);
 
     if (findResult.payload?.length <= 0) {
-        const insertResult = await insertMany({
-            collectionModel: School,
-            data: [
-                {
-                    name: 'Columbia University',
-                    city: 'New York City',
-                    score: 86.4,
-                    countryRegion: 'United States'
-                }
-            ]
-        });
-        console.log('insertResult => ', insertResult);
+        // const insertResult = await insertAsync({
+        //     collectionModel: School,
+        //     data: [
+        //         {
+        //             name: 'Columbia University',
+        //             city: 'New York City',
+        //             score: 86.4,
+        //             countryRegion: 'United States'
+        //         }
+        //     ]
+        // });
+        // console.log('insertResult => ', insertResult);
+    }
+
+    if (findResult.payload?.length > 0) {
+        // // Sample to delete a record found.
+        // console.log('record to be deleted (_id): ', findResult.payload[0]._id);
+        // const deleteResult = await deleteAsync({
+        //     collectionModel: School,
+        //     filter: {
+        //         _id: findResult.payload[0]._id
+        //     }
+        // });
+        // console.log('deleteResult => ', deleteResult);
+        
+        // // Sample to update a record found.
+        // console.log('records to be updated : ', findResult);
+        // const updateResult = await updateAsync({
+        //     collectionModel: School,
+        //     filter: {
+        //         countryRegion: "United States"
+        //     },
+        //     update: {
+        //         countryRegion: "U.S.A."
+        //     },
+        //     // multiple: true
+        // });
+        // console.log('updateResult => ', updateResult);
     }
     
 });
