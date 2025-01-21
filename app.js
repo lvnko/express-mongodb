67ec5bd9a386a0f8b0fa20ba6c8c956ab80dfa25
lvnko <kevinlao.lo@gmail.com>
2025-01-21 10:03:34 -0300

    CRUD 練習。

--- app.js
+++ app.js
@@ -26,6 +26,36 @@ const SchoolSchema = new mongoose.Schema({
 
 const School = mongoose.model('school', SchoolSchema, 'school');
 
+app.post("/school", async (req, res)=>{
+    const result = await School.create({
+        name: "National University of Singapore",
+        city: "Singapore",
+        countryRegion: "Singapore",
+        score: 83.5
+    });
+    console.log(result);
+    res.status(200).send(`Document of School is created!`);
+});
+
+app.delete("/school", async (req, res)=>{
+    const { id } = req.query;
+    const result = await School.deleteOne({
+        _id: id
+    });
+    res.status(200).send(`Document (_id: ${id}) is deleted!`);
+});
+
+app.put("/school", async (req, res)=>{
+    const { id, name, score, city, countryRegion } = req.query;
+    // On your curl command, encodeURI(...) maybe needed for url that contain information in Chinese.
+    const result = await School.updateOne({
+        _id: id
+    }, {
+        name, city, countryRegion, score
+    });
+    res.status(200).send(`Document (_id: ${id}) is updated!`);
+});
+
 app.get('/', async (req, res)=>{
     const schools = await School.find({});
     console.log(schools);
